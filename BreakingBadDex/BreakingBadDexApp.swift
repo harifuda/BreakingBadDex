a1757b5dffbfa9d92f044906a967e6f240d36165
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-11-04 10:07:47 -0400

    Initial Commit

---
+++ BreakingBadDex/BreakingBadDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadDexApp.swift
+//  BreakingBadDex
+//
+//  Created by Lukas Nguyen on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
